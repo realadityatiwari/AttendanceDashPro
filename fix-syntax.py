cf9e9bc0a296b4b57c1254db2095c580f51f484b
realadityatiwari <aditya360tiwari@gmail.com>
2026-08-09 13:38:08 +0530

    chore: remove automation scripts and test utilities, and add js/utils.js

--- fix-syntax.py
+++
@@ -1,9 +0,0 @@
-with open('js/attendance-engine.js', 'r', encoding='utf-8') as f:
-    c = f.read()
-
-c = c.replace('*/\n   INTERNAL ASSERTIONS', '/*\n   INTERNAL ASSERTIONS')
-
-with open('js/attendance-engine.js', 'w', encoding='utf-8') as f:
-    f.write(c)
-
-print("Fixed syntax error")
